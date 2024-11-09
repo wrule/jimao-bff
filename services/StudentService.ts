19ae7d7440d7c2ba3a4a72fcd21409d1b8083b31
jimao <shihao.gu@perfma.com>
2024-11-09 21:29:55 +0800

    一个新的service

---
+++ services/StudentService.ts
@@ -0,0 +1,16 @@
+
+class StudentService {
+  public getList() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          { name: '张三', age: 31 },
+          { name: '李四', age: 32 },
+          { name: '王二', age: 40 },
+        ]);
+      }, 1000);
+    });
+  }
+}
+
+export default StudentService;
