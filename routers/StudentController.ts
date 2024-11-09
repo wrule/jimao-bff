21e86f82027490f62046ddbd0971c6fccb4625de
jimao <shihao.gu@perfma.com>
2024-11-09 21:21:48 +0800

    提交

--- routers/StudentController.ts
+++ routers/StudentController.ts
@@ -1,12 +1,13 @@
 import { GET, route } from 'awilix-koa';
 import { IRouterContext } from 'koa-router';
 
-@route('/api')
+@route('/api/student')
 class StudentController {
-
   @route('/list')
   @GET()
   public list(ctx: IRouterContext, next: any) {
     ctx.body = { message: '1234' };
   }
 }
+
+export default StudentController;
