5604079d9439a0fc706f8b23a218cfdec62dba71
jimao <shihao.gu@perfma.com>
2024-11-09 10:54:33 +0800

    提交

--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
+import Koa from 'koa';
 
-console.log('你好，世界');
+async function main() {
+  console.log('你好，世界');
+  const app = new Koa();
+  app.use((ctx) => {
+    ctx.body = 'Hello Koa';
+  });
+  app.listen(3000);
+}
+
+main();
