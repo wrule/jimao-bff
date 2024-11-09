f1de1840d312da5698d77b1723fa89edf48658b8
jimao <shihao.gu@perfma.com>
2024-11-09 14:01:15 +0800

    复制过来ApiService

---
+++ services/ApiService.ts
@@ -0,0 +1,15 @@
+import { IApi } from '@/interfaces/IApi';
+import { IData } from '@/interfaces/IData';
+
+class ApiService implements IApi {
+  getInfo() {
+    return new Promise<IData>((resolve) => {
+      resolve({
+        item: '我是后台数据🌺',
+        result: [1, 'next'],
+      });
+    });
+  }
+}
+
+export default ApiService;
